c77f8f2429601d64e03c681c1c8a800d6f9c215e
Thomas Sayen <69324626+Chi-Iroh@users.noreply.github.com>
2025-04-21 23:12:41 +0200

    [WIP] Wrong special characters detection, TODO : correctly handling UTF-8 chars with Intl.Segmenter instead for ... of, as compound characters (like Guarani's g̃ (g + ~))

--- content.js
+++ content.js
@@ -1,9 +1,3 @@
-import(browser.runtime.getURL("./languages.js")).then(languages => {
-    if (isOnLossPage()) {
-        languages.displayHint();
-    }
-})
-
 function isOnMainPage() { // the game uses only a index.php page, so we must identify is we're on the main page to leave it untouched
     return document.getElementsByName("vyniki").length > 0; // there's a <a name="vyniki"> anchor to go to your results, if no vyniki then we're not in the main page (so in game)
 }
@@ -12,7 +6,10 @@ function isOnLossPage() { // wheen we lose a point, the correct answer and its w
     return document.getElementsByClassName("prawy ziel").length > 0; // prawy ziel and prawy czer classes only appear when we just lost a point
 }
 
-if (!isOnMainPage() && !isOnLossPage()) {
+console.log("hello")
+if (isOnLossPage()) {
+    window.displayHint(); // languages.js
+} else if (!isOnMainPage() && !isOnLossPage()) {
     const table = document.querySelector("table");
     setTimeout(() => {
         table.style.opacity = "0"; // still displayed but invisible, to avoid below buttons to move up
--- languages.js
+++ languages.js
@@ -1,9 +1,9 @@
 const writingSystems = new Map([
-    ["Hebrew", ["Hebrew", "Yiddish"]],
-    ["Bengali alphabet", ["Bengali", "Assamese"]],
-    ["Greek", ["Greek"]],
-    ["Georgian", ["Georgian"]],
-    ["Armenian", ["Armenian"]],
+    ["Hebrew alphabet", ["Hebrew", "Yiddish"]],
+    ["Bengali script", ["Bengali", "Assamese"]],
+    ["Greek alphabet", ["Greek"]],
+    ["Georgian alphabet", ["Georgian"]],
+    ["Armenian alphabet", ["Armenian"]],
     ["Cyrillic", [
         "Muscovite",
         "Ukrainian",
@@ -37,15 +37,15 @@ const writingSystems = new Map([
     ["Korean", ["Korean"]],
     ["Arabic", ["Arabic", "Persian", "Pashto"]],
     ["Inuktitut", ["Inuktitut"]],
-    ["Tamazight", ["Tamazight"]],
+    ["Tifinagh", ["Tamazight"]],
     ["Lao script", ["Lao"]],
-    ["Khmer script", ["Khmer"]],
-    ["Thai", ["Thai"]],
+    ["Khmer alphabet", ["Khmer"]],
+    ["Thai script", ["Thai"]],
     ["Cherokee", ["Cherokee"]],
     ["Amharic", ["Amharic"]],
-    ["Hindi", ["Hindi", "Nepali"]],
+    ["Devanāgarī", ["Hindi", "Nepali"]],
     ["Oriya", ["Oriya"]],
-    ["Punjabi", ["Punjabi"]],
+    ["Gurmukhi", ["Punjabi"]],
     ["Santali", ["Santali"]],
     ["Aramaic", ["Aramaic"]],
     ["Latin alphabet", [
@@ -79,29 +79,33 @@ const writingSystems = new Map([
         "Portuguese",
         "Czech",
         "Slovenian",
-        "Slovakian",
+        "Slovak",
+
         "Polish",
         "Kashubian",
         "Vilamovian",
         "Lower Sorbian",
         "Upper Sorbian",
+
         "Albanian",
         "Croatian",
         "Romanian",
         "Hungarian",
-        "Danish",
         "Norwegian",
         "Icelandic",
-        "Faroese",
         "Swedish",
-        "Greenlandic",
         "Finnish",
         "Estonian",
+        "Maltese",
+
+        "Danish",
+        "Faroese",
+        "Greenlandic",
+
         "Latvian",
         "Lithuanian",
         "Latgalian",
         "Livonian",
-        "Maltese",
 
         "Turkish",
         "Karakalpak",
@@ -138,14 +142,112 @@ const writingSystems = new Map([
     ["Telugu", ["Kannada", "Telugu"]],
     ["Sylheti", ["Sylheti"]],
     ["Malayalam", ["Malayalam"]],
-    ["Gujarati", ["Gujarati"]],
-    ["Tibetan", ["Tibetan"]],
+    ["Gujarati script", ["Gujarati"]],
+    ["Tibetan script", ["Tibetan"]],
     ["Tai Nuea", ["Tai Nuea"]],
     ["Nuosu", ["Nuosu"]],
     ["Old Church Slavonic", ["Old Church Slavonic"]],
     ["Burmese", ["Burmese"]],
     ["Osage", ["Osage"]]
 ]);
+// Crimean Tatar has some words in cyrillic, but everything else in latin alphabet
+// so that exception must be handled (isn't for now)
+
+const commonCharacters = new Map([
+    ["Latin alphabet", "abcdefghijklmnopqrstuvwxyz"],
+    ["Cyrillic alphabet", "АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШ"],
+    ["Arabic script", "أابتثجحخدذرزسشصضطظعغفقكلمنهؤوئىيء"],
+    ["Bengali script", "অআকাকিকীউকুঊকূঋকৃএকেঐকৈওকোঔকৌক ত্‍কংকঃকঁকখগঘঙচছজঝঞটঠডঢণতথদধনপফবভমযরৰলৱশষসহয়ড়ঢ়০১২৩৪৫৬৭৮৯"],
+    ["Devanāgarī", "अआइईउऊऋॠऌॡऍऎएऐऑऒओओकखगघङचछजझञटठडढणतथदधनपफबभमयरलळवशषसह०१२३४५६७८९प्पँपंपःप़पऽ"],
+    ["Gurmukhi", "ਅਆਇਈਉਊਏਐਓਔਕਖਗਘਙਚਛਜਝਞਟਠਡਢਣਤਥਦਧਨਪਫਬਭਮਯਰਲਲ਼ਵਸ਼ਸਹ"],
+    ["Gujarati script", "અઆઇઈઉઊઋઌઍએઐઑઓઔકખગઘઙચછજઝઞટઠડઢણતથદધનપફબભમયરલળવશષસહૠૡૢૣ"],
+    ["Tibetan script", "ཀཁགངཅཆཇཉཏཐདནཔཕབམཙཚཛཝཞཟའཡརལཤསཧཨ"],
+    ["Greek alphabet", "αβγδεζηθικλμνξοπρσςτυφχψω"],
+    ["Hebrew alphabet", "אבגדהוזחטיכלמנסעפצקרשת"],
+    ["Khmer alphabet", "កខគឃងចឆជឈញដឋឌឍណតថទធនបផពភមសហយរលឡអវអ្កអ្ខអ្គអ្ឃអ្ងអ្ចអ្ឆអ្ឈអ្ញអ្ឌអ្ឋអ្ឌអ្ឃអ្ណអ្តអ្ថអ្ទអ្ធអ្នអ្បអ្ផអ្ពអ្ភអ្មអ្សអ្ហអ្យអ្រអ្យអ្លអ្អអ្វអក្សរខ្មែរ"],
+    ["Armenian alphabet", "ԱԲԳԴԵԶԷԸԹԺԻԼԽԾԿՀՁՂՃՄՅՆՇՈՉՊՋՌՍՎՏՐՑՒՓՔՕՖ"],
+    ["Georgian alphabet", "აბგდევზჱთიკლმნჲოპჟრსტჳუფქღყშჩცძწჭხჴჯჰჵჶჷჸ"],
+    ["Thai script", "กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรฤลฦวศษสหฬอฮฯะา฿เแโใไๅๆ๏๐๑๒๓๔๕๖๗๘๙๚๛"],
+    ["Tifinagh", "ⴰⴱⴲⴳⴴⴵⴶⴷⴸⴹⴺⴻⴼⴽⴾⴿⵀⵁⵂⵃⵄⵅⵆⵇⵈⵉⵊⵋⵌⵍⵎⵐⵑⵒⵓⵔⵕⵖⵗⵘⵙⵚⵛⵜⵝⵞⵠⵡⵢⵣⵤⵥⵦⵧ"]
+]);
+
+const specialCharacters = new Map([
+    ["Dutch", "àäèéëïĳöü"],
+    ["West Frisian", "êôúû"],
+    ["Danish", "æøå"],
+    ["Norwegian", "æøå"],
+    ["Finnish", "äöšž"],
+    ["Swedish", "åäöé"],
+    ["Estonian", "äöõüšž"],
+    ["German", "äöüß"],
+    ["Kurdish", "çêîşû"],
+    ["Romanian", "ăâîșț"],
+    ["Welsh", "âêîôûŵŷáéíïóúẃýàèìòùẁỳäëöüẅÿ"],
+    ["Esperanto", "ĉĝĥĵŝŭ"],
+    ["Turkish", "çğıöşü"],
+    ["Icelandic", "áðéíóúýþæö"],
+    ["Faroese", "áðíóúýæø"],
+    ["Hungarian", "áéíóöőúüű"],
+    ["Catalan", "àçéèíóòúüï·"],
+    ["French", "àâæçéèêëîïôœùûüÿ"],
+    ["Portuguese", "ãõçáéíóúâêôà"],
+    ["Spanish", "áéíñóúü¡¿"],
+    ["Italian", "àéèìòù"],
+    ["Guarani", "áéíóúýãẽĩõũỹñg̃"],
+    ["Navajo", "áąą́éęę́íįį́łńóǫǫ́"],
+    ["Polish", "ąćęłńóśźż"],
+    ["Kashubian", "ąłńóżãéëòôù"],
+    ["Slovene", "šžč"],
+    ["Croatian", "šžčćđ"],
+    ["Czech", "šžčáďéěíňóřťúůý"],
+    ["Slovak", "šžčáäďéíľĺňóôŕťúý"],
+    ["Latvian", "šžčāēģīķļņōŗū"],
+    ["Lithuanian", "šžčąęėįųū"],
+    ["Vietnamese", "đàảãáạăằẳẵắặâầẩẫấậèẻẽéẹêềểễếệìỉĩíịòỏõóọồổỗốơờởỡớợùủũúụưừửữứựỳỷỹýỵ"],
+    ["Hawaiian", "āēīōū"],
+    ["Maori", "āēīōū"],
+    ["Basque", "ñ"],
+    ["Persian", "پچژگ"],
+    ["Yiddish", "פֿ"], // Add letters with dots
+
+    // Here are uppercase letters, must lower them
+    ["Bulgarian", "ЙЩЬЮЯЪ"],
+    ["Belarusian", "ЙЬЮЯЫЭёЎІ"],
+    ["Muscovite", "ЫЭёЪ"],
+    ["Ukrainian", "ҐЄІЇ"],
+    ["Macedonian", "ЉЊЏЈЃЌЅ"],
+    ["Serbian", "ЉЊЏЈЋЂ"]
+    // Cyrillic end
+]);
+
+var allSpecialCharacters = [];
+
+function initSpecialCharacters() {
+    for (const [language, _specialCharacters] of specialCharacters.entries()) {
+        for (const char of _specialCharacters) {
+            if (!allSpecialCharacters.includes(char)) {
+                allSpecialCharacters.push(char);
+                console.log(`${language} adds special character '${char}'`);
+            }
+        }
+    }
+    console.log(allSpecialCharacters)
+}
+initSpecialCharacters();
+
+function getSpecialCharacters(solution) {
+    let _specialCharacters = [];
+
+    for (const [englishWord, unknownWord] of solution.expectedWords.entries()) {
+        for (const char of unknownWord) {
+            if (allSpecialCharacters.includes(char)) {
+                _specialCharacters.push(char);
+            }
+        }
+    }
+    console.log("special characters", _specialCharacters, "in words", solution.expectedWords);
+    return _specialCharacters;
+}
 
 function alphabetOf(language) {
     for (let [alphabet, languages] of writingSystems.entries()) {
@@ -160,16 +262,16 @@ function parseSolution() {
     const table = document.querySelector("table");
     // table's 1st row = <empty space> | You've picked | Correct answer
     // table's 2nd row = <empty space> | French | Dutch
-    const languageToGuess = table.rows[1].cells[1].innerText;
-    const languageGuessed = table.rows[1].cells[2].innerText;
-    let expectedWords = {};
-    let guessedWords = {};
+    const languageGuessed = table.rows[1].cells[1].innerText;
+    const languageToGuess = table.rows[1].cells[2].innerText;
+    let expectedWords = [];
+    let guessedWords = [];
 
     for (let nthRow = 2; nthRow < table.rows.length; nthRow++) {
         const row = table.rows[nthRow];
         const word = row.cells[0].innerText;
-        expectedWords[word] = row.cells[1].innerText;
-        guessedWords[word] = row.cells[2].innerText;
+        guessedWords.push([word, row.cells[1].innerText]);
+        expectedWords.push([word, row.cells[2].innerText]);
     }
     return {
         languageToGuess: {
@@ -180,17 +282,33 @@ function parseSolution() {
             name: languageGuessed,
             alphabet: alphabetOf(languageGuessed)
         },
-        expectedWords: expectedWords,
-        guessedWords: guessedWords
+        expectedWords: new Map(expectedWords),
+        guessedWords: new Map(guessedWords)
     }
 }
 
-export function displayHint() {
+function specialCharactersHint(solution, visibleSpecialCharactersInLanguageToGuess) {
+    console.log("special chars in solution: ", visibleSpecialCharactersInLanguageToGuess)
+    const specialCharactersInGuessedLanguage = specialCharacters[solution.languageGuessed.name];
+    const hasGuessedLanguageNoSpecialCharacter = [null, undefined].includes(specialCharactersInGuessedLanguage);
+
+    for (const specialChar of visibleSpecialCharactersInLanguageToGuess) {
+        if (hasGuessedLanguageNoSpecialCharacter || !specialCharactersInGuessedLanguage.includes(specialChar)) {
+            return `${solution.languageGuessed.name} hasn't the special character '${specialChar}' !`;
+        }
+    }
+    return "";
+}
+
+window.displayHint = function () { // cannot use ES6 import/export for content scripts, so we define window constants. this script must appear before content.js in manifest.json
     const solution = parseSolution();
     let hintText = "";
     if (solution.languageToGuess.alphabet !== solution.languageGuessed.alphabet) {
-        hintText = `${solution.languageToGuess.name} use ${solution.languageToGuess.alphabet}, but ${solution.languageGuessed.name} use ${solution.languageGuessed.alphabet} instead !`;
+        hintText = `${solution.languageToGuess.name} uses ${solution.languageToGuess.alphabet}, but ${solution.languageGuessed.name} uses ${solution.languageGuessed.alphabet} instead !`;
+    } else {
+        hintText += specialCharactersHint(solution, getSpecialCharacters(solution));
     }
+    console.log(hintText)
     const hint = document.createTextNode(hintText);
     document.querySelector(".pojemnik").appendChild(hint);
     // form div containing both the table and the "next" button, so the hint is displayed beneath them
